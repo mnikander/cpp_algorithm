e3b207063ba353dfb568dbe767f037a21aca67d3
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-13 17:07:01 +0200

    add another example to the main

--- src/_main.cpp
+++ src/_main.cpp
@@ -2,14 +2,23 @@
 #include <iostream>
 #include "io/vector_stream.hpp"
 #include "meta/global_datatypes.hpp"
+#include "catenate.hpp"
 #include "drop.hpp"
+#include "iota.hpp"
+#include "map.hpp"
 #include "reverse.hpp"
 #include "take.hpp"
 
 int main()
 {
-    vi32 v = vi32{1, 2, 4, 8, 16} | xpr::take(3) | xpr::drop(1) | xpr::reverse();
+    using namespace xpr;
+
+    vi32 v = vi32{1, 2, 4, 8, 16} | take(3) | drop(1) | reverse();
     std::cout << "The answer: " << v << std::endl;
+
+    vi32 squares    = iota(5) | map([](int i){return i*i;});
+    vi32 palindrome = squares | catenate(squares | drop_last(1) | reverse());
+    std::cout << "Palidrome: " << palindrome << std::endl;
 
     return EXIT_SUCCESS;
 }
