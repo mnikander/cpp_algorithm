33c40fa9b93538d6109dae5c03220b429cd718c0
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-13 17:07:00 +0200

    rename logical operators to avoid collision with language keywords {and, or, not}

--- src/functional.hpp
+++ src/functional.hpp
@@ -6,9 +6,13 @@
 
 namespace nv {
 
+// Logical operators
+template <typename T> struct LogicalNot  { T operator()(T t) const { return !t; } };
+template <typename T> struct LogicalAnd  { T operator()(T lhs, T rhs) const { return lhs && rhs; } };
+template <typename T> struct LogicalOr   { T operator()(T lhs, T rhs) const { return lhs || rhs; } };
+
 // unary ops, which are both: (1) a tag which represents the operation and (2) the implementation of that operation
 template <typename T> struct Identity     { T operator()(T t) const { return t; } };
-template <typename T> struct Not          { T operator()(T t) const { return !t; } };
 template <typename T> struct Negate       { T operator()(T t) const { return -t; } };
 
 // binary ops, which are both: (1) a tag which represents the operation and (2) the implementation of that operation
@@ -26,25 +30,26 @@ template <typename T> struct LessEqual    { bool operator()(T lhs, T rhs) const 
 template <typename T> struct Greater      { bool operator()(T lhs, T rhs) const { return lhs > rhs; } };
 template <typename T> struct GreaterEqual { bool operator()(T lhs, T rhs) const { return lhs >= rhs; } };
 
-template <typename T> struct GreatestCommonDivisor { T operator()(T lhs, T rhs) const { return std::gcd(lhs,rhs); } };
-template <> struct GreatestCommonDivisor<bool> { bool operator()(bool lhs, bool rhs) const { return lhs || rhs; } };
-using Or = GreatestCommonDivisor<bool>; // OR is a specialization of GCD for booleans
-
 template <typename T> struct LeastCommonMultiple { T operator()(T lhs, T rhs) const { return std::lcm(lhs,rhs); } };
 template <> struct LeastCommonMultiple<bool> { bool operator()(bool lhs, bool rhs) const { return lhs && rhs; } };
-using And = LeastCommonMultiple<bool>; // AND is a specialization of LCM for booleans
+// note that AND is a specialization of LCM for booleans
 
-// combinators
-// TODO flip (i.e. C combinator) -- without function composition this is probably not that useful, since you'd be using
-// a lamdbda expression anyway, in the context of a map or map_reduce, for example
+template <typename T> struct GreatestCommonDivisor { T operator()(T lhs, T rhs) const { return std::gcd(lhs,rhs); } };
+template <> struct GreatestCommonDivisor<bool> { bool operator()(bool lhs, bool rhs) const { return lhs || rhs; } };
+// note that OR is a specialization of GCD for booleans
 
 }
 
-namespace xpr {
+namespace meta {
+
+// NEVER use these meta-programming tags and functions in client code
 
 // tags (without template parameters) which correspond to the above functors
+struct LogicalNot{};
+struct LogicalOr{};
+struct LogicalAnd{};
+
 struct Identity{};
-struct Not{};
 struct Negate{};
 struct Plus{};
 struct Minus{};
@@ -59,37 +64,78 @@ struct Less{};
 struct LessEqual{};
 struct Greater{};
 struct GreaterEqual{};
-struct GreatestCommonDivisor{};
-struct Or{};
 struct LeastCommonMultiple{};
-struct And{};
+struct GreatestCommonDivisor{};
 
-}
-
-namespace meta {
-
-// NEVER use these meta-programming functions in client code
 // dispatch function which converts the tag to the correct functor and sets the template T
 // intended to simplify the signatures of algorithms such as 'map'
-template <typename T> auto dispatch(xpr::Identity tag, T) { return nv::Identity<T>{}; }
-template <typename T> auto dispatch(xpr::Not tag, T) { return nv::Not<T>{}; }
-template <typename T> auto dispatch(xpr::Negate tag, T) { return nv::Negate<T>{}; }
-template <typename T> auto dispatch(xpr::Plus tag, T) { return nv::Plus<T>{}; }
-template <typename T> auto dispatch(xpr::Minus tag, T) { return nv::Minus<T>{}; }
-template <typename T> auto dispatch(xpr::Times tag, T) { return nv::Times<T>{}; }
-template <typename T> auto dispatch(xpr::Divides tag, T) { return nv::Divides<T>{}; }
-template <typename T> auto dispatch(xpr::Power tag, T) { return nv::Power<T>{}; }
-template <typename T> auto dispatch(xpr::Min tag, T) { return nv::Min<T>{}; }
-template <typename T> auto dispatch(xpr::Max tag, T) { return nv::Max<T>{}; }
-template <typename T> auto dispatch(xpr::Equal tag, T) { return nv::Equal<T>{}; }
-template <typename T> auto dispatch(xpr::NotEqual tag, T) { return nv::NotEqual<T>{}; }
-template <typename T> auto dispatch(xpr::Less tag, T) { return nv::Less<T>{}; }
-template <typename T> auto dispatch(xpr::LessEqual tag, T) { return nv::LessEqual<T>{}; }
-template <typename T> auto dispatch(xpr::Greater tag, T) { return nv::Greater<T>{}; }
-template <typename T> auto dispatch(xpr::GreaterEqual tag, T) { return nv::GreaterEqual<T>{}; }
-template <typename T> auto dispatch(xpr::GreatestCommonDivisor tag, T) { return nv::GreatestCommonDivisor<T>{}; }
-template <typename T> auto dispatch(xpr::LeastCommonMultiple tag, T) { return nv::LeastCommonMultiple<T>{}; }
-template <typename T> auto dispatch(xpr::And tag, T) { return nv::And{}; }
-template <typename T> auto dispatch(xpr::Or tag, T) { return nv::Or{}; }
+template <typename T> auto dispatch(LogicalNot tag, T) { return nv::LogicalNot<T>{}; }
+template <typename T> auto dispatch(LogicalAnd tag, T) { return nv::LogicalAnd<T>{}; }
+template <typename T> auto dispatch(LogicalOr tag, T) { return nv::LogicalOr<T>{}; }
+template <typename T> auto dispatch(Identity tag, T) { return nv::Identity<T>{}; }
+template <typename T> auto dispatch(Negate tag, T) { return nv::Negate<T>{}; }
+template <typename T> auto dispatch(Plus tag, T) { return nv::Plus<T>{}; }
+template <typename T> auto dispatch(Minus tag, T) { return nv::Minus<T>{}; }
+template <typename T> auto dispatch(Times tag, T) { return nv::Times<T>{}; }
+template <typename T> auto dispatch(Divides tag, T) { return nv::Divides<T>{}; }
+template <typename T> auto dispatch(Power tag, T) { return nv::Power<T>{}; }
+template <typename T> auto dispatch(Min tag, T) { return nv::Min<T>{}; }
+template <typename T> auto dispatch(Max tag, T) { return nv::Max<T>{}; }
+template <typename T> auto dispatch(Equal tag, T) { return nv::Equal<T>{}; }
+template <typename T> auto dispatch(NotEqual tag, T) { return nv::NotEqual<T>{}; }
+template <typename T> auto dispatch(Less tag, T) { return nv::Less<T>{}; }
+template <typename T> auto dispatch(LessEqual tag, T) { return nv::LessEqual<T>{}; }
+template <typename T> auto dispatch(Greater tag, T) { return nv::Greater<T>{}; }
+template <typename T> auto dispatch(GreaterEqual tag, T) { return nv::GreaterEqual<T>{}; }
+template <typename T> auto dispatch(LeastCommonMultiple tag, T) { return nv::LeastCommonMultiple<T>{}; }
+template <typename T> auto dispatch(GreatestCommonDivisor tag, T) { return nv::GreatestCommonDivisor<T>{}; }
 
 }
+
+namespace xpr {
+
+// function overloads: if there are NO arguments return the TAG, else if there ARE arguments execute the actual function
+
+// Unfortunately 'not' 'and' 'or' are reserved keywords in C++, so I cannot create functions with that name.
+// This led me to introduce the prefix 'logical' to all of the names. 
+                      auto logical_not() {return meta::LogicalNot{}; }                // get the tag
+template <typename T> auto logical_not(T const& t) { return nv::LogicalNot<T>{}(t); } // execute function
+                      auto logical_and() {return meta::LogicalAnd{}; }                                       // get the tag
+template <typename T> auto logical_and(T const& lhs, T const& rhs) { return nv::LogicalAnd<T>{}(lhs, rhs); } // execute function
+                      auto logical_or() {return meta::LogicalOr{}; }                                       // get the tag
+template <typename T> auto logical_or(T const& lhs, T const& rhs) { return nv::LogicalOr<T>{}(lhs, rhs); } // execute function
+
+// unary
+                      auto identity() {return meta::Identity{}; }                // get the tag
+template <typename T> auto identity(T const& t) { return nv::Identity<T>{}(t); } // execute function
+                      auto negate() {return meta::Negate{}; }                // get the tag
+template <typename T> auto negate(T const& t) { return nv::Negate<T>{}(t); } // execute function
+
+// binary
+                      auto least_common_multiple() {return meta::LeastCommonMultiple{}; }                                       // get the tag
+template <typename T> auto least_common_multiple(T const& lhs, T const& rhs) { return nv::LeastCommonMultiple<T>{}(lhs, rhs); } // execute function
+                      auto greatest_common_divisor() {return meta::GreatestCommonDivisor{}; }                                       // get the tag
+template <typename T> auto greatest_common_divisor(T const& lhs, T const& rhs) { return nv::GreatestCommonDivisor<T>{}(lhs, rhs); } // execute function
+
+
+}
+
+// Unfortunately 'not' 'and' 'or' are reserved keywords in C++, so I cannot create functions with that name.
+// Uppercase names would work.
+// To be consistent, I would have to give all my functors and functions uppercase names...
+// Or I force the user to differentiate between the tag 'Not' and the function 'Not'... that still doesn't change the
+// fact it cannot be lowercase though. :(
+// Another option would be to give them a special name by prefixing or postfixing something:
+// _not, not_f, f_not, not_, 
+
+// Specializing the the original functors to have zero-argument versions which are the tag, does not seem to work:
+//
+//     struct EmptyType {};
+//     template <typename T> struct Not  { T operator()(T t) const { return !t; } };
+//     template <> struct Not<EmptyType> {};
+//
+// results in compilation errors:
+//
+//     const auto t = Not<>{}; // error: wrong number of template arguments (0, should be 1)
+//     const auto t = Not{};   // error: class template argument deduction failed
+//
--- test/test_functional.cpp
+++ test/test_functional.cpp
@@ -5,14 +5,63 @@ namespace {
 
 TEST(functional, dispatch)
 {
-    const auto f = meta::dispatch(xpr::Identity{}, int());
+    const auto f = meta::dispatch(meta::Identity{}, int());
     EXPECT_EQ(f(5), 5);
 
-    const auto n = meta::dispatch(xpr::Negate{}, int());
+    const auto n = meta::dispatch(meta::Negate{}, int());
     EXPECT_EQ(n(5), -5);
 
-    const auto add = meta::dispatch(xpr::Plus{}, int());
+    const auto add = meta::dispatch(meta::Plus{}, int());
     EXPECT_EQ(add(2,7), 9);
+}
+
+TEST(functional, logical_not)
+{
+    const auto f = meta::dispatch(xpr::logical_not(), int());
+    EXPECT_EQ(f(false), true);
+    EXPECT_EQ(xpr::logical_not(false), true);
+}
+
+TEST(functional, logical_and)
+{
+    const auto f = meta::dispatch(xpr::logical_and(), int());
+    EXPECT_EQ(f(false, true), false);
+    EXPECT_EQ(xpr::logical_and(false, true), false);
+}
+
+TEST(functional, logical_or)
+{
+    const auto f = meta::dispatch(xpr::logical_or(), int());
+    EXPECT_EQ(f(false, true), true);
+    EXPECT_EQ(xpr::logical_or(false, true), true);
+}
+
+TEST(functional, identity)
+{
+    const auto f = meta::dispatch(xpr::identity(), int());
+    EXPECT_EQ(f(5), 5);
+    EXPECT_EQ(xpr::identity(5), 5);
+}
+
+TEST(functional, negate)
+{
+    const auto f = meta::dispatch(xpr::negate(), int());
+    EXPECT_EQ(f(5), -5);
+    EXPECT_EQ(xpr::negate(5), -5);
+}
+
+TEST(functional, lcm)
+{
+    const auto f = meta::dispatch(xpr::least_common_multiple(), int());
+    EXPECT_EQ(f(4, 6), 12);
+    EXPECT_EQ(xpr::least_common_multiple(4, 6), 12);
+}
+
+TEST(functional, gcd)
+{
+    const auto f = meta::dispatch(xpr::greatest_common_divisor(), int());
+    EXPECT_EQ(f(4, 6), 2);
+    EXPECT_EQ(xpr::greatest_common_divisor(4, 6), 2);
 }
 
 }
