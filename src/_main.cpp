808e3428749e89da29c782f2057175c22a52ed19
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-13 17:07:00 +0200

    use take and drop in main function

--- src/_main.cpp
+++ src/_main.cpp
@@ -1,14 +1,17 @@
 #include <cstdlib> // exit_success
 #include <iostream>
 #include <iterator> // ostream_iterator
+#include "drop.hpp"
 #include "reverse.hpp"
 #include "size.hpp"
+#include "take.hpp"
 #include "meta/datatype_aliases.hpp"
 
 int main()
 {
-    vi32 v = vi32{1, 2, 4, 8, 16} | xpr::reverse();
-    std::cout << "( ";
+    vi32 v = vi32{1, 2, 4, 8, 16} | xpr::take(3) | xpr::drop(1) | xpr::reverse();
+
+    std::cout << "The answer: ( ";
     std::copy(v.cbegin(), v.cend(), std::ostream_iterator<i32>(std::cout, " "));
     std::cout << ")" << std::endl;
 
