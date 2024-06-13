3646f424775b8dccad5a56e92ebdebc1fcc9036b
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-13 17:07:01 +0200

    xpr::rotate

--- src/rotate.hpp
+++ src/rotate.hpp
@@ -31,3 +31,28 @@ std::vector<T> rotate_last(std::vector<T> const& v, int count)
 }
 
 }
+
+namespace xpr {
+
+struct rotate : Expression<rotate>
+{
+    rotate(int count) : _count{count} {}
+
+    template<typename T>
+    std::vector<T> operator()(std::vector<T> const& v) const { return nv::rotate(v, _count); }
+
+    const int _count;
+};
+
+struct rotate_last : Expression<rotate_last>
+{
+    rotate_last(int count) : _count{count} {}
+
+    template<typename T>
+    std::vector<T> operator()(std::vector<T> const& v) const { return nv::rotate_last(v, _count); }
+
+    const int _count;
+
+};
+
+}
--- test/test_rotate.cpp
+++ test/test_rotate.cpp
@@ -51,4 +51,26 @@ TEST(rotate, all)
     EXPECT_EQ(result[4], 4);
 }
 
+TEST(rotate, xpr)
+{
+    vi32 result = xpr::iota(5) | xpr::rotate(2);
+    EXPECT_EQ(size(result), 5);
+    EXPECT_EQ(result[0], 2);
+    EXPECT_EQ(result[1], 3);
+    EXPECT_EQ(result[2], 4);
+    EXPECT_EQ(result[3], 0);
+    EXPECT_EQ(result[4], 1);
+}
+
+TEST(rotate_last, xpr)
+{
+    vi32 result = xpr::iota(5) | xpr::rotate_last(2);
+    EXPECT_EQ(size(result), 5);
+    EXPECT_EQ(result[0], 3);
+    EXPECT_EQ(result[1], 4);
+    EXPECT_EQ(result[2], 0);
+    EXPECT_EQ(result[3], 1);
+    EXPECT_EQ(result[4], 2);
+}
+
 }
