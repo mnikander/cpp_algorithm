b4f69eba62ac2dff0af227710e2e7cf3ac4fdd38
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-13 17:07:01 +0200

    allow different accmulator type in nv::reduce

--- src/reduce.hpp
+++ src/reduce.hpp
@@ -9,10 +9,11 @@
 
 namespace { // anonymous namespace
 
-template<typename T, typename OperatorTag> struct ReductionTraits{};
-template<typename T> struct ReductionTraits<T, nv::Plus> { static constexpr T init = T{0}; };
-template<typename T> struct ReductionTraits<T, nv::Max> { static constexpr T init = std::numeric_limits<T>::lowest(); };
-template<typename T> struct ReductionTraits<T, nv::Min> { static constexpr T init = std::numeric_limits<T>::max(); };
+template<typename T, typename BinaryOp> struct ReductionTraits{};
+template<typename T>                    struct ReductionTraits<T, nv::Plus>  { static constexpr T init = T{0}; };
+template<typename T>                    struct ReductionTraits<T, nv::Times> { static constexpr T init = T{1}; };
+template<typename T>                    struct ReductionTraits<T, nv::Max>   { static constexpr T init = std::numeric_limits<T>::lowest(); };
+template<typename T>                    struct ReductionTraits<T, nv::Min>   { static constexpr T init = std::numeric_limits<T>::max(); };
 
 // TODO: do I want to implement alternating minus and division reductions? is that even possible with a normal reduction?
 //       the alternating minus might be useful for calculating determinants
@@ -22,8 +23,8 @@ template<typename T> struct ReductionTraits<T, nv::Min> { static constexpr T ini
 namespace nv {
 
 // having 'op' as an argument, like the STL, and not just as a template argument, enables easy use of lambda expressions
-template<typename T, typename BinaryOp>
-T reduce(std::vector<T> v, BinaryOp op, T initialValue = ReductionTraits<T, BinaryOp>::init)
+template<typename T, typename BinaryOp, typename Accumulator = T>
+T reduce(std::vector<T> v, BinaryOp op, Accumulator initialValue = ReductionTraits<T, BinaryOp>::init)
 {
     return std::reduce(v.cbegin(), v.cend(), initialValue, op);
 }
