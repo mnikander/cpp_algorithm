37b9935c312d0d70bbf6f1e9fdfd2267d0704c0c
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-13 17:07:00 +0200

    xpr::catenate

--- src/catenate.hpp
+++ src/catenate.hpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <vector>
+#include "meta/expression.hpp"
 #include "size.hpp"
 
 namespace nv {
@@ -38,5 +39,32 @@ std::vector<T> catenate(T const& left, T const& right)
 {
     return std::vector<T>{left, right};
 }
+
+}
+
+namespace xpr {
+
+template <typename T>
+struct catenate : Expression<catenate<T>>
+{
+    catenate(T const& right) : _right{right} {}
+
+    std::vector<T> operator()(std::vector<T> const& left) const { return nv::catenate(left, _right); }
+    std::vector<T> operator()(T const& left) const { return nv::catenate(left, _right); }
+
+    const T _right;
+};
+
+// template specialization for std::vector
+template <typename T>
+struct catenate<std::vector<T>> : Expression<catenate<std::vector<T>>>
+{
+    catenate(std::vector<T> const& right) : _right{right} {}
+
+    std::vector<T> operator()(std::vector<T> const& left) const { return nv::catenate(left, _right); }
+    std::vector<T> operator()(T const& left) const { return nv::catenate(left, _right); }
+
+    const std::vector<T> _right;
+};
 
 }
--- test/test_catenate.cpp
+++ test/test_catenate.cpp
@@ -55,4 +55,44 @@ TEST(catenate, element_element)
     EXPECT_EQ(result[1], 42);
 }
 
+TEST(catenate, xpx_vector_vector)
+{
+    vi32 result = vi32{0, 1, 2} | xpr::catenate(vi32{2, 1, 0});
+    ASSERT_EQ(size(result), 6);
+    EXPECT_EQ(result[0], 0);
+    EXPECT_EQ(result[1], 1);
+    EXPECT_EQ(result[2], 2);
+    EXPECT_EQ(result[3], 2);
+    EXPECT_EQ(result[4], 1);
+    EXPECT_EQ(result[5], 0);
+}
+
+TEST(catenate, xpr_vector_element)
+{
+    vi32 result = vi32{0, 1, 2} | xpr::catenate(42);
+    ASSERT_EQ(nv::size(result), 4);
+    EXPECT_EQ(result[0], 0);
+    EXPECT_EQ(result[1], 1);
+    EXPECT_EQ(result[2], 2);
+    EXPECT_EQ(result[3], 42);
+}
+
+TEST(catenate, xpr_element_vector)
+{
+    vi32 result = 42 | xpr::catenate(vi32{0, 1, 2});
+    ASSERT_EQ(nv::size(result), 4);
+    EXPECT_EQ(result[0], 42);
+    EXPECT_EQ(result[1], 0);
+    EXPECT_EQ(result[2], 1);
+    EXPECT_EQ(result[3], 2);
+}
+
+TEST(catenate, xpr_element_element)
+{
+    vi32 result = 13 | xpr::catenate(42);
+    ASSERT_EQ(nv::size(result), 2);
+    EXPECT_EQ(result[0], 13);
+    EXPECT_EQ(result[1], 42);
+}
+
 }
