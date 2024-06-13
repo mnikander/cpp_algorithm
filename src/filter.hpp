e53ecf8956e5092169aeb8051d29026ef2f40508
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-13 17:07:00 +0200

    xpr::filter

--- src/filter.hpp
+++ src/filter.hpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <iterator>
 #include <vector>
+#include "meta/expression.hpp"
 #include "size.hpp"
 
 namespace nv {
@@ -14,5 +15,20 @@ std::vector<T> filter(std::vector<T> const& v, UnaryPred op)
     std::copy_if(v.cbegin(), v.cend(), std::back_inserter(result), op);
     return result;
 }
+
+}
+
+namespace xpr {
+
+template <typename UnaryPred>
+struct filter : Expression<filter<UnaryPred>>
+{
+    filter(UnaryPred pred) : _pred{pred} {}
+
+    template<typename T>
+    std::vector<T> operator()(std::vector<T> const& v) const { return nv::filter(v, _pred); }
+
+    UnaryPred const _pred;
+};
 
 }
--- test/test_filter.cpp
+++ test/test_filter.cpp
@@ -53,4 +53,14 @@ TEST(filter, odd)
     EXPECT_EQ(result[1], 3);
 }
 
+TEST(filter, xpr)
+{
+    vi32 result = vi32{0, 1, 2, 3, 4} | xpr::filter([](int i){ return i % 2 == 0; });
+
+    ASSERT_EQ(size(result), 3);
+    EXPECT_EQ(result[0], 0);
+    EXPECT_EQ(result[1], 2);
+    EXPECT_EQ(result[2], 4);
+}
+
 }
