8a10e6fb9ee6221e94347d8ec15dd9965dc61af6
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-13 17:07:00 +0200

    add binary overloads for nv::map

--- src/map.hpp
+++ src/map.hpp
@@ -7,11 +7,37 @@
 namespace nv {
 
 template<typename T, typename UnaryOp>
-auto map(std::vector<T> const& data, UnaryOp op) -> std::vector<decltype(op(T{}))>
+auto map(std::vector<T> const& left, UnaryOp op) -> std::vector<decltype(op(T{}))>
 {
-    std::vector<decltype(op(T{}))> result(size(data));
-    std::transform(data.cbegin(), data.cend(), result.begin(), op);
+    std::vector<decltype(op(T{}))> result(size(left));
+    std::transform(left.cbegin(), left.cend(), result.begin(), op);
+    return result;
+}
+
+template <typename T, typename BinaryOp>
+auto map(T left, BinaryOp op, std::vector<T> const& right) -> std::vector<decltype(op(T{}, T{}))>
+{
+    std::vector<T> result(size(right));
+    std::transform(right.cbegin(), right.cend(), result.begin(), [op, &left](T const& x){ return op(left, x); });
+    return result;
+}
+
+template <typename T, typename BinaryOp>
+auto map(std::vector<T> const& left, BinaryOp op, T right) -> std::vector<decltype(op(T{}, T{}))>
+{
+    std::vector<T> result(size(left));
+    std::transform(left.cbegin(), left.cend(), result.begin(), [op, &right](T const& x){ return op(x, right); });
+    return result;
+}
+
+template <typename T, typename BinaryOp>
+auto map(std::vector<T> const& left, BinaryOp op, std::vector<T> const& right) -> std::vector<decltype(op(T{}, T{}))>
+{
+    assert(size(left) == size(right));
+    std::vector<T> result(size(left));
+    std::transform(left.cbegin(), left.cend(), right.cbegin(), result.begin(), op);
     return result;
 }
 
 }
+
