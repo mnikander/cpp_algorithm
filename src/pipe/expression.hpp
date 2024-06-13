52cfa113f0c5467b1fb3daae96d6a94ddce100f9
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-13 17:07:00 +0200

    pipe operator supports scalar and vector-valued results

--- src/pipe/expression.hpp
+++ src/pipe/expression.hpp
@@ -31,7 +31,7 @@ struct Expression{};
 // }
 
 template<typename T, typename E>
-T operator|(std::vector<T> const& v, Expression<E> const& right)
+auto operator|(std::vector<T> const& v, Expression<E> const& right) -> decltype((*static_cast<const E*>(&right))(v))
 {
     const auto expression = *(static_cast<const E*>(&right)); // downcasting to the exact type of expression via CRTP
     return (expression.operator()(v));
